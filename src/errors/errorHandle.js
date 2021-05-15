8a4f7bc9d4602bcd1f0589e4c38c1263b73ee876
Victor Klimov <4klimov@gmail.com>
2021-05-15 22:13:52 +0300

    refactor: add badRequest check to errorHandle.js

--- src/errors/errorHandle.js
+++ src/errors/errorHandle.js
@@ -1,8 +1,10 @@
 const NOT_FOUND_ERROR = require('./notFoundError');
+const BAD_REQUEST_ERROR = require('./badRequestError');
 
 const errorHandle = async (err, req, res, next) => {
   console.error(err.stack);
-  if (err instanceof NOT_FOUND_ERROR) {
+  if (err instanceof NOT_FOUND_ERROR ||
+      err instanceof BAD_REQUEST_ERROR) {
     res.status(err.status).send(err.message);
   } else if (err) {
     res.status(500).send('Some error happened!');
