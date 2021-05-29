95169af36941ffd7cc06fbcb81817faf8049fbc2
Victor Klimov <4klimov@gmail.com>
2021-05-30 02:49:11 +0300

    docs: add jsdoc to the rest of the app

--- src/common/config.js
+++ src/common/config.js
@@ -5,10 +5,19 @@ dotenv.config({
   path: path.join(__dirname, '../../.env')
 });
 
-module.exports = {
+/**
+ * Environment variables
+ * @typedef {Object.<string, string|boolean>} EnvVar
+ */
+/**
+ * @type {EnvVar}
+ */
+const ENV_VAR = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   AUTH_MODE: process.env.AUTH_MODE === 'true'
 };
+
+module.exports = ENV_VAR;
