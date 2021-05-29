a9d526d3d9ea81a5becb7825dc7eae7ea52512db
Victor Klimov <4klimov@gmail.com>
2021-05-30 02:47:49 +0300

    docs: add jsdoc description to async wrapper

--- src/utils/wrapAsync.js
+++ src/utils/wrapAsync.js
@@ -1,3 +1,8 @@
+/**
+ * Checks if fn is classified as a Function object.
+ * @param {Function} fn 
+ * @returns {boolean}
+ */
 function isFunction(fn) {
   return (
     fn &&
@@ -5,7 +10,12 @@ function isFunction(fn) {
       {}.toString.call(fn) === '[object AsyncFunction]')
   );
 }
-
+/**
+ * Wrapper for handling async errors
+ * @param {Function} fn 
+ * @returns {Function}
+ * @throws {Error}
+ */
 function wrapRoute(fn) {
   if (!isFunction(fn)) {
     throw new Error(`fn ${fn.name} should be a function`);
@@ -17,7 +27,11 @@ function wrapRoute(fn) {
     }
   };
 }
-
+/**
+ * The input can be a function or an array of functions
+ * @param {Function|Array<Function>} obj - callback or array of callbacks
+ * @returns {Function|Array<Function>}
+ */
 function wrapAsync(obj) {
   if (Array.isArray(obj)) {
     return obj.map(wrapAsync);
